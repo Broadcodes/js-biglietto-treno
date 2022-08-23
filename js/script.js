cad933c6ee6d4cd3a1367d14b43059a20a38ae68
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-23 14:56:04 +0200

    Creazione delle costanti

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+const PREZZO_UNITARIO_BIGLIETTO = 0.21;
+const SCONTO_MINORENNI = 20;
+const SCONTO_OVER65 = 40;
+
 var kilometri = parseInt(prompt("Quanti kilometri vuoi percorrere con il treno oggi?"));
 var anni = parseInt(prompt("Quanti anni hai?"));
 
 if(!isNaN(kilometri) || !isNaN(anni)){
-    console.log(`${kilometri} e ${anni}`);
+    var risultatoMomentaneo = (`${kilometri} e ${anni}`);
+
+
+
+    document.getElementById("calcoloPrezzo").innerHTML = risultatoMomentaneo;
 } else {
     alert("ATTENZIONE: Il valore inserito non è corretto, aggiornare la pagina");
 }
