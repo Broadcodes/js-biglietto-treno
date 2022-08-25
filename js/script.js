62c321cb7191170e7c395fc1ebb98d2e55c36179
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-08-25 09:12:42 +0200

    Correzione variabili da var a let

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ const SCONTO_MINORENNI = 20;
 const SCONTO_OVER65 = 40;
 
 // variabili necessarie a memorizzare i dati chiesti all'utente
-var kilometri = parseInt(prompt("Quanti kilometri vuoi percorrere con il treno oggi?"));
-var anni = parseInt(prompt("Quanti anni hai?"));
+const kilometri = parseInt(prompt("Quanti kilometri vuoi percorrere con il treno oggi?"));
+const anni = parseInt(prompt("Quanti anni hai?"));
 
 // condizione per verificare se il dato inserito sia realmente un numero e non una stringa.
 // in caso di errore mostra un messaggio all'utente
 if(!(isNaN(kilometri) || isNaN(anni))){
     // calcolo il prezzo del biglietto per i km inseriti dall'utente
-    var prezzoBigliettoDaAcquistare = PREZZO_UNITARIO_BIGLIETTO * kilometri;
+    let prezzoBigliettoDaAcquistare = PREZZO_UNITARIO_BIGLIETTO * kilometri;
     // dichiaro una variabile che conterrà il prezzo finale del biglietto
-    var prezzoBigliettoFinale;
+    let prezzoBigliettoFinale;
 
     // condizione che permette di applicare lo sconto in base all'età dell'utente
     if(anni < ETA_MINIMA){
